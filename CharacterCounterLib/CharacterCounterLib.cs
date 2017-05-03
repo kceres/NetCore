e7e9658d0318ca9b53cdf1043807cf79726d0bcd
Danilo Cáceres <kceres.at@gmail.com>
2017-05-03 13:35:02 -0300

    .Net Core
    All about

---
+++ CharacterCounterLib/CharacterCounterLib.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class CharacterCounter
+    {
+        readonly Dictionary<char, int> _counts = new Dictionary<char, int>();
+        readonly char[] _alphabet = "abcdefghijklmnopqrstuvxyz".ToCharArray();
+
+        public CharacterCounter()
+        {
+            foreach (var letter in _alphabet)
+                _counts.Add(letter, 0);
+        }
+
+        public void ParseString(string phrase)
+        {
+            char[] charactersInPhrase = phrase.ToLower().ToCharArray();
+
+            foreach (var letter in charactersInPhrase)
+            {
+                if (Char.IsLetter(letter))
+                    _counts[letter] = _counts[letter] + 1;
+            }
+        }
+
+        public int GetCountForLetter(char letter){
+            return _counts[letter];
+        }
+    }
+}
---
+++ test/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using Library;
+using FluentAssertions;
+
+namespace CharacterCounterTests
+{
+    public class GetCountForLetter
+    {
+        [Fact]
+        public void Should_return_a_count_of_1_if_there_is_a_single_letter_to_parse()
+        {
+            CharacterCounter counter = new CharacterCounter();
+            counter.ParseString("a");
+            Assert.Equal(1,counter.GetCountForLetter('a'));
+            counter.GetCountForLetter('a').Should().Be(1);//FluentAssertions
+        }
+    }
+}
